7135a1e82488fcc464a184301ab583c59c8f6626
Alex Stuart <alex.stuart@jisc.ac.uk>
2018-03-30 17:09:43 +0100

    Add feature to list the registrationAuthority strings in the file

--- composition.pl
+++ composition.pl
@@ -12,11 +12,12 @@ sub usage {
 
         print <<EOF;
 
-	usage: $0 [-h] [ -reg <regAuth> ]
-
-	<regAuth> - only output entities from this registrationAuthority
+	usage: $0 [-h] [ -reg <regAuth> ] [-l] <file>
 
 	-h - print this help text and exit
+
+	<regAuth> - only output entities from this registrationAuthority
+	-l        - list the registrationAuthority strings in this file
 
 	Given an EntityDescriptor or EntitiesDescriptor, display the elements
 	that are in this file. Outputs number of occurences.
@@ -26,8 +27,10 @@ EOF
 
 my $help;
 my $registrationAuthority;
+my $list;
 GetOptions (	"help" => \$help,
-		"reg=s" => \$registrationAuthority
+		"reg=s" => \$registrationAuthority,
+		"list" => \$list
 	   );
 
 if ( $help ) {
@@ -52,6 +55,20 @@ my $dom = XML::LibXML->load_xml( location => $xmlfile);
 my $xpc = XML::LibXML::XPathContext->new( $dom );
 $xpc->registerNs( 'md', 'urn:oasis:names:tc:SAML:2.0:metadata' );
 $xpc->registerNs( 'mdrpi', 'urn:oasis:names:tc:SAML:metadata:rpi' );
+
+if ( $list ) {
+	my @nodes = $xpc->findnodes( '//md:EntityDescriptor/md:Extensions/mdrpi:RegistrationInfo/@registrationAuthority' );
+	foreach (@nodes) {
+		my $regAuth = ${_}->to_literal;
+		#print "$regAuth\n";
+		++$frequency{ $regAuth };
+	}
+	foreach (sort { $frequency{ $b } <=> $frequency{ $a } } keys %frequency) {
+		print "$_ $frequency{ $_ }\n";
+	}
+	exit 0;
+}
+
 if ( $registrationAuthority ) {
 	@nodes = $xpc->findnodes( '//md:EntityDescriptor[md:Extensions/mdrpi:RegistrationInfo/@registrationAuthority="' . $registrationAuthority . '"]');
 } else {
